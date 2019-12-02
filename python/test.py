93d61815fdfc95401136cfdd968732df0b42bcd3
jiphan <james.phan@revature.net>
2019-12-02 12:28:32 -0500

    citations

--- python/test.py
+++ python/test.py
@@ -1,8 +1,11 @@
 memo = {0:0, 1:1}
 def fibm(n):
+    # https://www.geeksforgeeks.org/python-program-for-program-for-fibonacci-numbers-2/
     # if not n in memo:
     #     memo[n] = fibm(n-1) + fibm(n-2)
     # return memo[n]
+
+    # https://stackoverflow.com/questions/15047116/an-iterative-algorithm-for-fibonacci-numbers
     a, b = 0, 1
     for i in range(0, n):
         a, b = b, a + b
