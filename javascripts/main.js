069cbe8cddb7969c0d88cdb400cfb440acc72126
jiphan <james.phan@revature.net>
2019-12-02 14:45:22 -0500

    stable handlebars example

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,11 @@
 console.log('This would be the main JS file.');
+$(function () {
+    var body = $("#p1").html();
+    var template = Handlebars.compile(body);
+    var context = {
+        text1: 'Hello',
+        text2: "world!" 
+    };
+    var body2 = template(context);
+    $("#p1").html(body2);
+});
