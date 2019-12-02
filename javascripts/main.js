45ec8280c2fa1921127185b1a6588231fcab8606
jiphan <james.phan@revature.net>
2019-12-02 15:21:34 -0500

    added loud helper

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,9 +3,13 @@ $(function () {
     var body = $("#p1").html();
     var template = Handlebars.compile(body);
     var context = {
-        text1: 'Hello',
-        text2: "world!" 
+        text1: "Hello",
+        text2: "world!",
+        text3: "something"
     };
+    Handlebars.registerHelper('loud', function(aString) {
+        return aString.toUpperCase();
+    });
     var body2 = template(context);
     $("#p1").html(body2);
 });
