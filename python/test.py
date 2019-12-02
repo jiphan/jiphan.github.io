c2d30db555ffb3063d410cc931068ce5f117243f
jiphan <james.phan@revature.net>
2019-12-02 12:26:29 -0500

    iterative fibonacci

--- python/test.py
+++ python/test.py
@@ -1,8 +1,13 @@
 memo = {0:0, 1:1}
 def fibm(n):
-    if not n in memo:
-        memo[n] = fibm(n-1) + fibm(n-2)
-    return memo[n]
+    # if not n in memo:
+    #     memo[n] = fibm(n-1) + fibm(n-2)
+    # return memo[n]
+    a, b = 0, 1
+    for i in range(0, n):
+        a, b = b, a + b
+    return a
+
 
 print("hello world")
 print(fibm(int(input('Fibonacci number: '))))
