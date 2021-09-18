84174fbe2bd649459eead58770617e7eb467e1f2
jiphan <jiphan@umich.edu>
2021-09-18 17:53:15 -0500

    deployment files

--- deploy.sh
+++ deploy.sh
@@ -12,12 +12,12 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-# git init
+git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:jiphan/jiphan.github.io.git main
+git push -f https://github.com/jiphan/jiphan.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
