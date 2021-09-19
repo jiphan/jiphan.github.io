66bb42bd94d76cb28d88ab285fc4513412fe3988
jiphan <jiphan@umich.edu>
2021-09-19 00:07:41 -0500

    changing to hash mode history

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Current from '../views/Current.vue'
 import NotFound from '../views/NotFound.vue'
 
@@ -40,7 +40,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 })
 router.beforeEach((to, from, next) => {
