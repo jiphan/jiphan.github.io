e48dcdd565a0cef7cb170b287bc5f1549a851604
jiphan <jiphan@umich.edu>
2021-09-19 00:45:55 -0500

    updating About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="about">
-    <h2>This is an about page</h2>
+  <h3>This is an about page</h3>
+  <div class="comp">
+    <a href="https://github.com/jiphan/jiphan.github.io">github</a>
   </div>
 </template>
