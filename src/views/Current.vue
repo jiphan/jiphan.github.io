69535ac91565d613e56aed0490d8e697749dd780
jiphan <jiphan@umich.edu>
2021-09-19 21:38:22 -0500

    added stock chart

--- src/views/Current.vue
+++ src/views/Current.vue
@@ -1,13 +1,16 @@
 <template>
   <postItem class="comp" @add-row="addRow" />
+  <tradingview/>
 </template>
 
 <script>
 import postItem from "@/components/postItem.vue";
+import tradingview from "@/components/tradingview.vue";
 export default {
   name: "Dev",
   components: {
     postItem,
+    tradingview
   },
   methods: {
     addRow() {
