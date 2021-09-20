59464e08a3fc68f9e66a5ddb7561dd29b0723919
jiphan <jiphan@umich.edu>
2021-09-19 23:04:04 -0500

    class binding on bold

--- src/components/item.vue
+++ src/components/item.vue
@@ -1,54 +1,54 @@
 <template>
-  <div :class="post.bold ? 'emphasis' : ''" class="show">
-    <span class="">{{ post.msg }}</span>
-    <span class="hide">
-      <i class="fas fa-bold" @click="$emit('bold-row')" />
-      <i class="far fa-copy" @click="copyTC(post.msg)" />
-      <i
-        :class="post.bold ? 'fas fa-lock' : 'fas fa-times'"
-        @click="$emit('del-row', post)"
-      />
-    </span>
-  </div>
+	<div v-bind:class="{ emphasis: post.bold }" class="show">
+		<span class="">{{ post.msg }}</span>
+		<span class="hide">
+			<i class="fas fa-bold" @click="$emit('bold-row')" />
+			<i class="far fa-copy" @click="copyTC(post.msg)" />
+			<i
+				:class="post.bold ? 'fas fa-lock' : 'fas fa-times'"
+				@click="$emit('del-row', post)"
+			/>
+		</span>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'Item',
-  props: {
-    post: Object
-  },
-  methods: {
-    copyTC(text) {
-      navigator.clipboard.writeText(text)
-    }
-  }
+	name: 'Item',
+	props: {
+		post: Object
+	},
+	methods: {
+		copyTC(text) {
+			navigator.clipboard.writeText(text)
+		}
+	}
 }
 </script>
 
 <style scoped>
 div {
-  display: flex;
-  justify-content: space-between;
-  font: 14px sans-serif;
-  text-decoration: none;
+	display: flex;
+	justify-content: space-between;
+	font: 14px sans-serif;
+	text-decoration: none;
 }
 .emphasis {
-  font-weight: bold;
+	font-weight: bold;
 }
 i {
-  padding: 0px 5px;
-  cursor: pointer;
+	padding: 0px 5px;
+	cursor: pointer;
 }
 span {
-  overflow-wrap: break-word;
-  max-width: 80%;
-  text-align: left;
+	overflow-wrap: break-word;
+	max-width: 80%;
+	text-align: left;
 }
 span.hide {
-  display: none;
+	display: none;
 }
 .show:hover .hide {
-  display: inline;
+	display: inline;
 }
 </style>
