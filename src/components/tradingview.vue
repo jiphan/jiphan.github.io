407d22a94d8aed19c77b2c23d53b8a6a0bbe8e72
jiphan <jiphan@umich.edu>
2021-09-20 22:40:47 -0500

    AAPL -> SPY

--- src/components/tradingview.vue
+++ src/components/tradingview.vue
@@ -1,34 +1,34 @@
 <template>
-  <!-- TradingView Widget BEGIN -->
-  <div class="tradingview-widget-container">
-    <div id="tradingview_53906"></div>
-  </div>
-  <!-- TradingView Widget END -->
+	<!-- TradingView Widget BEGIN -->
+	<div class="tradingview-widget-container">
+		<div id="tradingview_53906"></div>
+	</div>
+	<!-- TradingView Widget END -->
 </template>
 
 <script>
 export default {
-  mounted() {
-    let tradingviewScript = document.createElement("script")
-    tradingviewScript.setAttribute("src", "https://s3.tradingview.com/tv.js")
-    document.head.appendChild(tradingviewScript)
+	mounted() {
+		let tradingviewScript = document.createElement("script")
+		tradingviewScript.setAttribute("src", "https://s3.tradingview.com/tv.js")
+		document.head.appendChild(tradingviewScript)
 
-    setTimeout(function () {
-      new window.TradingView.widget({
-        width: "auto",
-        height: 480,
-        symbol: "NASDAQ:AAPL",
-        interval: "D",
-        timezone: "Etc/UTC",
-        theme: "dark",
-        style: "1",
-        locale: "en",
-        toolbar_bg: "#f1f3f6",
-        enable_publishing: false,
-        allow_symbol_change: true,
-        container_id: "tradingview_53906",
-      })
-    }, 100)
-  },
+		setTimeout(function () {
+			new window.TradingView.widget({
+				width: "auto",
+				height: 480,
+				symbol: "NASDAQ:SPY",
+				interval: "D",
+				timezone: "Etc/UTC",
+				theme: "dark",
+				style: "1",
+				locale: "en",
+				toolbar_bg: "#f1f3f6",
+				enable_publishing: false,
+				allow_symbol_change: true,
+				container_id: "tradingview_53906",
+			})
+		}, 100)
+	},
 };
 </script>
